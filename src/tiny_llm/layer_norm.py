c584dd6b121385f3e8def302523ec34d5db33f99
Shawn Xia <shawn.xia@duolingo.com>
2026-04-06 22:21:46 -0400

    Week 1 Day 4

--- src/tiny_llm/layer_norm.py
+++ src/tiny_llm/layer_norm.py
@@ -3,7 +3,12 @@ import mlx.core as mx
 
 class RMSNorm:
     def __init__(self, dim: int, weight: mx.array, eps: float = 1e-5):
-        pass
+        self.dim = dim
+        self.weight = weight
+        self.eps = eps
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
+        # Note that, mean calculation should be performed with float32 accumulation to maintain precision before taking the square root, even if the input and weights are in a lower precision format (e.g., float16 or bfloat16).
+        x = x.astype(mx.float32)
+        # keepdims for broadcast back
+        return x / (mx.sqrt(mx.mean(x**2, axis=-1, keepdims=True)) + self.eps) * self.weight
