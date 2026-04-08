9ac08369c04eb6f239a2226a1927c6e27b19ac5d
Shawn Xia <shawn.xia@duolingo.com>
2026-04-07 22:25:27 -0400

    Week 1 Day 7 2

--- src/tiny_llm/sampler.py
+++ src/tiny_llm/sampler.py
@@ -8,12 +8,15 @@ def make_sampler(temp: float, top_p: float | None, top_k: int | None):
         if temp == 0:
             return mx.argmax(logprobs, axis=-1)
         if top_k is not None:
-            top_k_idx = mx.argpartition(-logprobs, top_k-1)
-            logprobs[top_k_idx[top_k:]] = float('-inf')
-        if top_p is not None:
-            sorted_idx = mx.argsort(-logprobs) # IDX!
-            cumsum = mx.cumsum(mx.exp(logprobs[sorted_idx]))
-            logprobs[sorted_idx[cumsum > top_p]] = float('-inf')
+            top_k_idx = mx.argpartition(-logprobs, top_k-1, axis=-1)
+            logprobs[top_k_idx[:, top_k:]] = float('-inf')
+        if top_p is not None and top_p > 0:
+            sorted_idx = mx.argsort(-logprobs, axis=-1)
+            sorted_logprobs = logprobs[:, sorted_idx]
+            cumsum = mx.cumsum(mx.exp(sorted_logprobs), axis=-1)
+            mask = cumsum < top_p
+            mask[..., 0] = True
+            logprobs[:, sorted_idx] = mx.where(mask, sorted_logprobs, -mx.inf)
         return mx.random.categorical(logprobs/temp, -1)
 
     return sample
